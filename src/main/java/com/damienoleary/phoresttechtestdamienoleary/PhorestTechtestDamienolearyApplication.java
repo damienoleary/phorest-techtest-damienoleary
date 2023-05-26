f87455b8b84553a10f22555b45828a7213c7b7fb
Damien O'Leary <damienoleary@outlook.com>
2023-05-26 23:54:36 +0100

    Initial commit

---
+++ src/main/java/com/damienoleary/phoresttechtestdamienoleary/PhorestTechtestDamienolearyApplication.java
@@ -0,0 +1,13 @@
+package com.damienoleary.phoresttechtestdamienoleary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhorestTechtestDamienolearyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhorestTechtestDamienolearyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damienoleary/phoresttechtestdamienoleary/PhorestTechtestDamienolearyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damienoleary.phoresttechtestdamienoleary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhorestTechtestDamienolearyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
