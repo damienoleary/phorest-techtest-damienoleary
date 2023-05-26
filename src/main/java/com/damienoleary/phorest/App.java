8775824147dd9ffb4b6783a99bc7a2b0f5ed086b
Damien O'Leary <damienoleary@outlook.com>
2023-05-26 23:57:11 +0100

    Renaming generated classes/packages

--- src/main/java/com/damienoleary/phorest/App.java
+++ src/main/java/com/damienoleary/phorest/App.java
@@ -1,13 +1,13 @@
-package com.damienoleary.phoresttechtestdamienoleary;
+package com.damienoleary.phorest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhorestTechtestDamienolearyApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhorestTechtestDamienolearyApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
